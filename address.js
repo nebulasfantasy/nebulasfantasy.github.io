b9ba523c7f7c8f02fff31b2c26ab363347423432
yukairex <wyktju@gmail.com>
2018-06-10 20:15:15 -0400

    tested on testnet, and switched to mainnet

--- address.js
+++ address.js
@@ -1,5 +1,7 @@
 // const contractAddress = "n1f2AouN2cqwXpBUTNTja1ciH3yT126rbem";
 // const mainnet = false;
 
+
+
 const contractAddress = "n1euf1q5Ptf8VxQh2LMJZvSLs4gT6QR29Lx";//74590f054cec9821b7db249fac4b2a5ada63169a814c55dcb6aa100095277b29
 const mainnet = true;
